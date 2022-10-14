2a7684b2bd24a696dc643a1efc7106c25050f356
StrigZ <mirgasan2010@ya.ru>
2022-10-14 17:49:31 +0400

    fix about

--- pages/About.tsx
+++ pages/About.tsx
@@ -6,7 +6,7 @@ type Props = {};
 const About = (props: Props) => {
   return (
     <motion.div
-      className=" h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
+      className="relative  flex  flex-col h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center "
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
@@ -27,34 +27,28 @@ const About = (props: Props) => {
         transition={{
           duration: 1.5,
         }}
-        className="flex-shrink-0 "
+        className="-mb-20 md:mb-0 flex-shrink-0 w-36 h-36 rounded-full object-cover sm:w-32  md:rounded-lg md:w-64 md:h-72 xl:w-[500px] xl:h-[600px] "
       >
         <Image
           src={profilePic}
           alt=""
-          className=" w-56 h-56 -mb-20 md:mb-0  md:w-64 md:h-95 xl:w-[500px] xl:h-[600px] md:rounded-lg  rounded-full object-cover "
+          className="-mb-20 md:mb-0 flex-shrink-0 w-full h-full rounded-full object-cover   md:rounded-lg "
         />
       </motion.div>
 
       <div className="space-y-10 px-0 md:px-10">
-        <h4 className="text-4xl font-semibold">
+        <h4 className="text-3xl md:text-4xl font-semibold">
           Here is a{" "}
           <span className="underline decoration-[#f7ab0a]/50">little</span>{" "}
           background
         </h4>
         <p className="text-base">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea dicta odit
-          eligendi illum nam! Laboriosam, maiores! Labore earum quidem optio
-          quibusdam exercitationem facere omnis iste eaque, nam accusantium
-          delectus expedita eius. Debitis fugiat doloremque libero nesciunt unde
-          nulla, earum dolorem fuga aliquid, voluptatum vel tenetur dignissimos
-          magnam architecto voluptatibus reiciendis quod. Eveniet nobis dolorem
-          a ea eum voluptatum porro omnis sed quisquam ad totam veritatis,
-          expedita nisi accusamus quasi, suscipit aperiam. Natus repellendus
-          voluptatem distinctio deleniti asperiores ullam est omnis at, mollitia
-          iusto facere cum perferendis dicta aliquam sit nostrum, officia
-          voluptatibus in consequatur assumenda? Incidunt repudiandae sint
-          voluptatem eaque!
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
+          perspiciatis, officia, unde iusto alias, maiores natus eveniet ipsum
+          asperiores quidem error? Rem, eius nesciunt. Quasi veniam fugiat
+          provident rem praesentium ratione, tempore officiis sapiente ducimus
+          esse ipsum molestiae quae velit laudantium mollitia incidunt quaerat
+          ad voluptate. Libero possimus mollitia doloremque?
         </p>
       </div>
     </motion.div>
