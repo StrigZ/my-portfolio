c42595bad564e87bd8fa3ce17d69c7b45234ae6d
StrigZ <mirgasan2010@ya.ru>
2022-10-20 19:31:04 +0400

    add responsive to contactme section

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -1,13 +1,16 @@
 import { PhoneIcon, MapPinIcon, EnvelopeIcon } from "@heroicons/react/24/solid";
 import { useForm, SubmitHandler } from "react-hook-form";
-type Props = {};
+import { PageInfo } from "../typings";
+type Props = {
+  contactme: PageInfo;
+};
 type Inputs = {
   name: string;
   email: string;
   subject: string;
   message: string;
 };
-const ContactMe = (props: Props) => {
+const ContactMe = ({ contactme }: Props) => {
   const { register, handleSubmit } = useForm<Inputs>();
   const onSubmit: SubmitHandler<Inputs> = (formData) => {
     window.location.href = `mailto:${formData.email}?subject=${formData.subject}?body=${formData.message}?name=${formData.name}`;
@@ -18,32 +21,34 @@ const ContactMe = (props: Props) => {
         Contact
       </h3>
 
-      <div className="flex flex-col space-y-10">
-        <h4 className="text-base sm:text-xl md:text-4xl font-semibold text-center">
+      <div className="flex flex-col space-y-10 mt-28">
+        <h4 className="text-lg sm:text-2xl md:text-4xl font-semibold text-center tracking-wide ">
           I have got just what you need.{" "}
-          <span className="decoration-[#f7ab0a]/50 underline">Lets Talk.</span>
+          <span className="mt-1 block decoration-[#f7ab0a]/50 underline">
+            Lets Talk.
+          </span>
         </h4>
 
-        <div className="space-y-10">
-          <div className="flex items-center space-x-5 justify-center">
+        <div className="space-y-5 md:space-y-10">
+          {/* <div className="flex items-center space-x-5 justify-center">
             <PhoneIcon className="text-[#f7ab0a] h-7 w-7 animate-pulse" />
-            <p className="text-2xl">Number Placeholder</p>
-          </div>
+            <p className="text-xl sm:text-2xl">{contactme?.phoneNumber}</p>
+          </div> */}
           <div className="flex items-center space-x-5 justify-center">
             <EnvelopeIcon className="text-[#f7ab0a] h-7 w-7 animate-pulse" />
-            <p className="text-2xl">mirgasan200@gmail.com</p>
+            <p className="text-xl sm:text-2xl">{contactme?.email}</p>
           </div>
-          <div className="flex items-center space-x-5 justify-center">
+          {/* <div className="flex items-center space-x-5 justify-center">
             <MapPinIcon className="text-[#f7ab0a] h-7 w-7 animate-pulse" />
-            <p className="text-2xl">Location Placeholder</p>
-          </div>
+            <p className="text-xl sm:text-2xl">{contactme?.address}r</p>
+          </div> */}
         </div>
 
         <form
           onSubmit={handleSubmit(onSubmit)}
           className="flex flex-col space-y-2 w-fit mx-auto"
         >
-          <div className="flex space-x-2">
+          <div className="flex flex-col space-y-2 sm:flex-row sm:space-y-0 sm:space-x-2">
             <input
               className="contactInput"
               {...register("name")}
@@ -72,7 +77,7 @@ const ContactMe = (props: Props) => {
             className="bg-[#f7ab0a] py-5 px-10 rounded-md text-black font-bold text-lg "
             type="submit"
           >
-            Submit
+            Send
           </button>
         </form>
       </div>
