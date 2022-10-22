f71d539a79a28da6820961183fc81cd1a1fd4332
StrigZ <mirgasan2010@ya.ru>
2022-10-22 19:42:24 +0400

    fix font on md screens

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -49,7 +49,7 @@ const Projects = ({ projects }: Props) => {
                 </motion.div>
               </a>
               <div className="space-y-10 px-0 md:px-10 max-w-6xl">
-                <h4 className="text-xl xl:text-4xl font-semibold text-center">
+                <h4 className="text-xl md:text-2xl xl:text-4xl font-semibold text-center">
                   <span className="underline decoration-[#f7aboa]/50">
                     Case Study {i + 1} of {projects.length}
                   </span>
